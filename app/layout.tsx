75936bac3e34a213b058f13281677e5d2505ea7c
edwintantawi <edwintantawi.workspace@gmail.com>
2023-07-04 21:58:49 +0700

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Foodsery',
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon.ico', sizes: '16x16' },
+      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+    ],
+  },
+};
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
