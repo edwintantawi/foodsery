b9bcca67fb9946158c84b914f462fb4849fa1359
edwintantawi <edwintantawi.workspace@gmail.com>
2023-07-04 21:57:00 +0700

    feat: setup nextjs with typescript

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
