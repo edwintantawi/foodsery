4e4ff07cb8133937296eb0f8f82abc9472b8558a
edwintantawi <edwintantawi.workspace@gmail.com>
2023-07-08 13:42:44 +0700

    fix(ui): z-index overlap

--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from '~/components/search-bar';
 
 export function AppBar() {
   return (
-    <nav className="sticky top-0 flex items-center gap-2 border-b p-2 lg:gap-3 lg:p-3">
+    <nav className="sticky top-0 z-50 flex items-center gap-2 border-b bg-background p-2 lg:gap-3 lg:p-3">
       <Brand />
       <SearchBar />
     </nav>
