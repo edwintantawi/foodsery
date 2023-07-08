4fbb5d4190dfc8f6bf469011dd7c3ae375123e1f
edwintantawi <edwintantawi.workspace@gmail.com>
2023-07-08 16:28:38 +0700

    refactor: simplify brand component

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,25 +1,17 @@
-import * as React from 'react';
-import NextLink from 'next/link';
+import Link from 'next/link';
 
 import { Icons } from '~/components/icons';
 import { Button } from '~/components/ui/button';
-import { cn } from '~/lib/utils';
 
-interface BrandProps {
-  className?: string;
+export function Brand() {
+  return (
+    <div className="flex w-fit items-center gap-1.5 text-primary">
+      <Button asChild size="icon">
+        <Link href="/">
+          <Icons.Brand />
+          <span className="sr-only">Foodsery</span>
+        </Link>
+      </Button>
+    </div>
+  );
 }
-
-export const Brand = React.forwardRef<HTMLAnchorElement, BrandProps>(
-  function Brand({ className }, ref) {
-    return (
-      <div className="flex w-fit items-center gap-1.5 text-primary">
-        <Button asChild size="icon">
-          <NextLink ref={ref} href="/" className={cn(className)}>
-            <Icons.Brand />
-            <span className="sr-only">Foodsery</span>
-          </NextLink>
-        </Button>
-      </div>
-    );
-  }
-);
