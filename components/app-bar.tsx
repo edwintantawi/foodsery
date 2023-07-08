c8de05f312b506316209f08ae06b67fe8f17c5e4
edwintantawi <edwintantawi.workspace@gmail.com>
2023-07-08 16:32:11 +0700

    fix: useSearchParams causes root page become CSR

--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { Brand } from '~/components/brand';
 import { SearchBar } from '~/components/search-bar';
 
@@ -5,7 +7,9 @@ export function AppBar() {
   return (
     <nav className="sticky top-0 z-50 flex items-center gap-2 border-b bg-background p-2 lg:gap-3 lg:p-3">
       <Brand />
-      <SearchBar />
+      <React.Suspense fallback={<div className="h-10 w-full" />}>
+        <SearchBar />
+      </React.Suspense>
     </nav>
   );
 }
