0d5aeac69aa6310171ca08629dca269f482cf9b8
edwintantawi <edwintantawi.workspace@gmail.com>
2023-07-10 16:13:32 +0700

    feat: add containers dir to tailwindcss content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './containers/**/*.{ts,tsx}',
+  ],
   theme: {
     container: {
       center: true,
